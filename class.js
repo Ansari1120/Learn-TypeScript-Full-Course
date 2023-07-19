95d4ebc564e6e5c19d6ba5f554809af207a1a9b0
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-07-20 01:43:56 +0500

    "typescript class code and docs added"

--- class.js
+++ class.js
@@ -15,11 +15,105 @@
 // display_name.display(); 
 // console.log(display_name); //using old values , can be double assigned.
 //strtict checking is enabled
-var Person = /** @class */ (function () {
-    function Person(std_roll, std_name) {
-        this.name = "adbul sami", //using class properties under constructor
-            this.roll = 23;
-        console.log("".concat(this.roll));
+// class Person { //making a class
+//     roll:number; //setting properties , exclamation can be used | , optional ? making if not often needed , 
+//     name:string; //want to declare a peropert which wont to be changed later on use readonly keyword then
+//     constructor(std_roll:number,std_name:string){ //make a constructor with parameters
+//      this.name = "adbul sami",  //using class properties under constructor
+//      this.roll = 23;
+//      console.log(`${this.roll}`);
+//     }
+//  }
+//
+//interface implements and class both simultaneously used : 
+// interface learner{
+//     course_name:string
+//  }
+//  interface book {
+//     book_name:string
+//  }
+//  class Another implements learner , book{
+//     id = 11
+//     course_name = "OOP"
+//     book_name = "java"
+//     constructor(course : string){ //can be initialized values outside or inside constructor 
+//        this.book_name             //to remember object 
+//        this.course_name = course
+//     }
+//  }
+//  let var_name = new Another("my course");
+//  console.log(var_name);
+//class method 
+//class method
+// class Greet { 
+//     do_something(num : number , num1:number){ //method
+//         let sum = num+num1;
+//         console.log(`a number ${sum} got remainder !`);
+//         return sum;
+//     }
+// }
+// let obj = new Greet();
+// obj.do_something(7,15);
+//class property
+// class Tour {
+//     destination : string
+//     constructor(destination:string){
+//         this.destination = destination;
+//     }
+// }
+// let obj1 = new Tour("maldives");
+// obj1.destination
+//function property
+// class Func_property{
+//     taskes_parameters = (input:boolean) => input? "yes" : "no";
+// }
+// const choose_either = new Func_property();
+// choose_either.taskes_parameters(true) //say either true or false
+//strict checking
+// class Check{
+//     immediate = 0;
+//     earlier : string;
+//     post : number|undefined;
+//     pre : number;  // shows an error since the value coulb be undefined/null
+//     constructor(){
+//         this.earlier = "nothing"; //assgined there if not above when writting property
+//     }
+// }
+//Example 02 : 
+//exclamation option checking
+// class ActivitiesQueue {
+// pending!: string[]; // Ok  // since the property dont need to be explicity undefined we make it optional through exclamation !
+// initialize(pending: string[]) {
+// this.pending = pending;
+// }
+// next() {
+// return this.pending.pop();
+// }
+// }
+// const activities = new ActivitiesQueue();
+// activities.initialize(['eat', 'sleep', 'learn'])
+// activities.next();
+// optional property
+var Queue = /** @class */ (function () {
+    function Queue() {
     }
-    return Person;
+    Queue.prototype.initialize = function (pending) {
+        this.pending = pending;
+    };
+    Queue.prototype.next = function () {
+        return this.adding.pop();
+    };
+    return Queue;
 }());
+var obj2 = new Queue();
+obj2.initialize(['eat', 'sleep', 'learn']);
+obj2.next();
+// readonly property
+var A_quote = /** @class */ (function () {
+    function A_quote(quote) {
+        this.quote = quote;
+    }
+    return A_quote;
+}());
+var see = new A_quote("friend in need is a firend indeed");
+// see.quote="wont make changes" //since the property is readonly.
